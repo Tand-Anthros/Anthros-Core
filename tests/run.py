b4d6470962c912367df2aefc5002749b9f57a6e5
Tand <thunder.light.pegasys@gmail.com>
2022-10-06 18:25:42 +0300

    commit

--- tests/run.py
+++ tests/run.py
@@ -1,16 +1,18 @@
-from anthros import core as ac
+#from anthros import core as ac
+#Не работает запуск AC, поправить
 import os, sys
 
 if sys.platform in ['win32']: slash = '\\'
 else: slash = '/'
-pos = str(__file__).split(slash)
+
+if '\\' in str(__file__): pos = str(__file__).split('\\')
+else: pos = str(__file__).split('/')
 pos = slash.join(pos[:len(pos) - 2] + ['src', 'anthros'])
 
 sys.path[0] = pos
 os.chdir(pos)
 if sys.platform == 'win32':
     os.system(pos.split(slash)[:1][0])
-
 
 import core as ac
 ac.interfaces.console.run()
